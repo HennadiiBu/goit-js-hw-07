0b838d78204ca659aefd6caf311b894e68a265e5
Hennadii Bublikov <bublikov.gennadiy@gmail.com>
2023-03-29 20:23:07 +0300

    task-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,8 +1,44 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-const gallery = document.querySelector(".gallery")
-gallery.classList.add('gallery')
-const markup = galleryItems.map((item) => `<li class="gallery__item"><img class="gallery__image" src=${item.preview} alt=${item.description}></li>`)
-gallery.insertAdjacentHTML('beforeend', markup.join(""));
+const gallery = document.querySelector(".gallery");
+gallery.classList.add("gallery");
+(function () {
+  const markup = galleryItems.map(
+    (image) =>
+      `<li class="gallery__item js-target ">
+  <a class="gallery__link js-target" >
+    <img
+      class="gallery__image js-target js-card"
+      src=${image.preview}
+      data-source=${image.original}
+      alt=${image.description}
+    />
+  </a>
+</li>`
+  );
+  gallery.insertAdjacentHTML("beforeend", markup.join(""));
+})();
 
+gallery.addEventListener("click", onClick);
+
+function onClick(event) {
+  if (!event.target.classList.contains("js-target")) {
+    return;
+  }
+
+  const adress = event.target.closest(".js-card");
+  const { source } = adress.dataset;
+  const data = galleryItems.find(({ original }) => original === source);
+
+  const instance = basicLightbox.create(`
+      <img
+        class="gallery__image"
+        src=${data.original}
+       
+        alt=${data.description}
+      />
+  `);
+
+  instance.show();
+}
