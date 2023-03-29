ac40a9fd1a4b8755b2dd59df76ac577bb76ad125
Hennadii Bublikov <bublikov.gennadiy@gmail.com>
2023-03-29 18:49:33 +0300

    task-1 Markup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,8 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery")
+gallery.classList.add('gallery')
+const markup = galleryItems.map((item) => `<li class="gallery__item"><img class="gallery__image" src=${item.preview} alt=${item.description}></li>`)
+gallery.insertAdjacentHTML('beforeend', markup.join(""));
+
